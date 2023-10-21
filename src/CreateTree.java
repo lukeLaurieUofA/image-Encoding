275a519a082a9ab9e16d0bc29404b3c32cd8d245
Desmondtga <Desmondtga@arizona.edu>
2023-10-20 19:41:01 -0700

    added comment

--- src/CreateTree.java
+++ src/CreateTree.java
@@ -1,6 +1,7 @@
 public class CreateTree {
 
 	public static void main(String[] args) {
+		// :)
 		HuffmanTree huffmanTree = new HuffmanTree();
 		Node testNode = huffmanTree.huffmanEncoding(null);
 	}
