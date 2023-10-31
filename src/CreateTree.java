35f3cc30fd1850b7a0ea5f81f77bb8a51997a9f4
jacobedelany <jacobedelany@arizona.edu>
2023-10-30 19:43:34 -0700

    Testing git

--- src/CreateTree.java
+++ src/CreateTree.java
@@ -2,6 +2,9 @@ public class CreateTree {
 
 	public static void main(String[] args) {
 		// :)
+		// :(
+		// yo 
+		// ahh
 		HuffmanTree huffmanTree = new HuffmanTree();
 		Node testNode = huffmanTree.huffmanEncoding(null);
 	}
