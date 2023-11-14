314b08cbd29f306a6f5d12cf91efddf969ad06c6
lukelaurie <lukelaurie13@gmail.com>
2023-11-13 19:07:22 -0700

    added comments

--- src/CreateTree.java
+++ src/CreateTree.java
@@ -1,3 +1,12 @@
+
+/**
+ * This will do the conversion from the array of bytes from the image 
+ * then will convert it into the proper form to run the encoding algorithm 
+ * on It will also then use the tree to convert each of the bytes into their 
+ * correct compressed forms. 
+ * Date: 10/20/2023
+ */
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.PriorityQueue;
@@ -7,7 +16,7 @@ public class CreateTree {
 	public static void main(String[] args) {
 		// load in the images
 		byte[] imageBytes = ImageBitExtraction.getBites("smileyface.jpg");
-		ArrayList<Node> theNodes = addNodesToMap(imageBytes);
+		ArrayList<Node> theNodes = addNodesToList(imageBytes);
 		PriorityQueue<Node> pQueue = addNodesToQueue(theNodes);
 		// create a tree from the frequencies
 		HuffmanTree huffmanTree = new HuffmanTree();
@@ -18,29 +27,56 @@ public class CreateTree {
 		System.out.println(encodedBytes);
 	}
 
+	/**
+	 * This will take all of the encoding mappings from the map, and convert each of
+	 * the bytes into their correct compressed forms.
+	 * 
+	 * @param imageBytes       is the bytes from the image.
+	 * @param encodingMappings Is the map containing all of the encodings.
+	 * 
+	 * @return The list of the encoded strings.
+	 */
 	private static ArrayList<String> encodeBytes(byte[] imageBytes, HashMap<Byte, String> encodingMappings) {
-		ArrayList<String> encodedBytes = new ArrayList<>(); 
+		ArrayList<String> encodedBytes = new ArrayList<>();
 		// adds all the encoded paths to the list
-		for (Byte aByte : imageBytes) { 
+		for (Byte aByte : imageBytes) {
 			String path = encodingMappings.get(aByte);
 			encodedBytes.add(path);
 		}
 		return encodedBytes;
 	}
 
+	/**
+	 * This will take use the tree and map all the needed values into their correct
+	 * paths in the tree.
+	 * 
+	 * @param huffmanTree Is the tree with the methods to get the path.
+	 * @param root        Is the root of the tree.
+	 * @param imageBytes  Is the bytes from the image.
+	 * 
+	 * @return The map containing the path mappings.
+	 */
 	private static HashMap<Byte, String> encodeBytesIntoMap(HuffmanTree huffmanTree, Node root, byte[] imageBytes) {
 		HashMap<Byte, String> encodingMappings = new HashMap<>();
-		for (Byte aByte : imageBytes) { 
+		for (Byte aByte : imageBytes) {
 			// only searches and adds to the tree when not yet exist
 			if (!encodingMappings.containsKey(aByte)) {
-				String path = huffmanTree.findPathToNode(aByte, root); 
+				String path = huffmanTree.findPathToNode(aByte, root);
 				encodingMappings.put(aByte, path);
 			}
 		}
 		return encodingMappings;
 	}
 
-	private static ArrayList<Node> addNodesToMap(byte[] imageBytes) {
+	/**
+	 * This will add all of the nodes to a list and keep track of the frequencies
+	 * for each of the nodes.
+	 * 
+	 * @param imageBytes Is the bytes from the image.
+	 * 
+	 * @return The list of nodes.
+	 */
+	private static ArrayList<Node> addNodesToList(byte[] imageBytes) {
 		// generate a list of nodes containing the frequencies
 		ArrayList<Node> theNodes = new ArrayList<>();
 		for (Byte aByte : imageBytes) {
@@ -56,6 +92,14 @@ public class CreateTree {
 		return theNodes;
 	}
 
+	/**
+	 * This will add all of the nodes into a priority queue based on their specified
+	 * frequencies.
+	 * 
+	 * @param theNodes Is the list of nodes containing the frequencies.
+	 * 
+	 * @return The priority queue contains all the nodes in the correct order.
+	 */
 	private static PriorityQueue<Node> addNodesToQueue(ArrayList<Node> theNodes) {
 		PriorityQueue<Node> pQueue = new PriorityQueue<Node>(new NodeComparator());
 		for (Node curNode : theNodes) {
@@ -64,6 +108,14 @@ public class CreateTree {
 		return pQueue;
 	}
 
+	/**
+	 * This will find the node specified the the byte.
+	 * 
+	 * @param theNodes Is the list of nodes containing the frequencies.
+	 * @param aByte Is the byte to search for.
+	 * 
+	 * @return The found Nodes
+	 */
 	private static Node findNode(ArrayList<Node> theNodes, byte aByte) {
 		for (Node curNode : theNodes) {
 			if (curNode.getValue() == aByte)
