61583869e362287dfb7759cd249f9a937f4b0993
lukelaurie <lukelaurie13@gmail.com>
2023-11-20 10:32:14 -0700

    code

--- src/WriteToFile.java
+++ src/WriteToFile.java
@@ -1,13 +1,6 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 /**
  * write method will write an encoding to a target file
  */
