d630b0026a35483cb232ea26a59c78403a5520b6
jacobedelany <jacobedelany@arizona.edu>
2023-11-20 17:09:26 -0700

    started test code

---
+++ src/ProgramTesting.java
@@ -0,0 +1,9 @@
+
+public class ProgramTesting {
+	public static void main(String args[]) {
+		System.out.println("Test 01:  Ensuring  that the mask is able is accurate ");
+		int a = 255;
+		int b = 123;
+		int c = 123;
+	}
+}
